0d2f2a14df3590a775c56bc0f466562d21f8ed5f
Jayandra Hamal <salonhamal278@gmail.com>
2024-02-01 21:34:31 +0530

    frontend complete

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+
+import { WorkoutsContextProvider } from './context/WorkoutsContext.jsx';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WorkoutsContextProvider>
+      <App />
+    </WorkoutsContextProvider>
   </React.StrictMode>,
 )
